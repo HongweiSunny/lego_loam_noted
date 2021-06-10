923a448c1da4a1a6a7d90304f965adc08b359b97
HongweiSunny <2043500880@qq.com>
2021-06-10 23:29:38 +0800

    add all file 210610

---
+++ LeGO-LOAM/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/shw/catkin_ws_legoloam/src/LeGO-LOAM/LeGO-LOAM/build/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/shw/catkin_ws_legoloam/src/LeGO-LOAM/LeGO-LOAM/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/shw/catkin_ws_legoloam/src/LeGO-LOAM/LeGO-LOAM/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/shw/catkin_ws_legoloam/src/LeGO-LOAM/LeGO-LOAM:$ROS_PACKAGE_PATH"
